df4b5baf673a25d597b2dea7752fc369f58bd7f3
Jack <erfeng.cheng@dianping.com>
2015-11-19 16:39:26 +0800

    Project Init

---
+++ JKPhotoListDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKPhotoListDemo
+//
+//  Created by Jack on 15/11/19.
+//  Copyright © 2015年 宇之楓鷙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
